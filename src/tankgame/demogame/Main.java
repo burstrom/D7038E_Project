588ef762ba683916a9b5c61e7a019f80cd8edca4
sirfrog <sirfrog@jameswin>
2015-12-13 19:22:01 +0100

    Initial commit. Setting up framework.

---
+++ src/tankgame/demogame/Main.java
@@ -0,0 +1,48 @@
+package tankgame.demogame;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.RenderManager;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+
+/**
+ * Some notes on the game:
+ * Playing field is a square that's 600x600 units.
+ * Tanks are 8 long and 6 wide boxes, with a turret on top.
+ * 
+ */
+
+public class Main extends SimpleApplication {
+
+    public 
+    
+    public static void main(String[] args) {
+        Main app = new Main();
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        Box b = new Box(1, 1, 1);
+        Geometry geom = new Geometry("Box", b);
+
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setColor("Color", ColorRGBA.Blue);
+        geom.setMaterial(mat);
+
+        rootNode.attachChild(geom);
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        //TODO: add update code
+    }
+
+    @Override
+    public void simpleRender(RenderManager rm) {
+        //TODO: add render code
+    }
+}
